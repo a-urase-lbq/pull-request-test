8c0fea17de188dd041c40d07a478d1e985fa2fd0
a-urase-lbq <a.urase@lberc-g.jp>
2024-08-28 12:22:32 +0900

    レビュー内容反映

--- sort.js
+++ sort.js
@@ -2,7 +2,7 @@ var number = [19, 3, 81, 1, 24, 21];
 console.log(number);
 var sortnumber = function (number) {
     number.sort(function (a, b) {
-        if (a == b) {
+        if (a === b) {
             return 0;
         }
         return a < b ? -1 : 1;
